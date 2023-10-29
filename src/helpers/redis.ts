75df537a19d1353194528da98b10a6055f64a8f4
Yixsi <yixsi3103@gmail.com>
2023-10-28 21:44:46 -0500

    added fn to fetch the info from redis

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,4 +1,25 @@
 const dbUrl = process.env.UPSTASH_REDIS_REST_URL;
 const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
 
-type Commands = 'zrange' | 'sismember' | 'smembers' | 'get';
+type Command = 'zrange' | 'sismember' | 'smembers' | 'get';
+
+export async function fetchRedis(
+	command: Command,
+	...args: (string | number)[]
+) {
+	const commandURL = `${dbUrl}/${command}/${args.join('/')}`;
+
+	const response = await fetch(`${commandURL}`, {
+		headers: {
+			Authorization: `Bearer ${authToken}`
+		},
+		cache: 'no-store'
+	});
+
+	if (!response.ok) {
+		throw new Error(`Error executing command: ${response.statusText}`);
+	}
+
+	const data = await response.json();
+	return data.result;
+}
